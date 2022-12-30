0368936e379146b1668bd6219c5592f6d63b2408
dai <truongcongdai25@gmail.com>
2022-12-30 13:10:41 +0700

    update

--- customeraddons/exam1/models/approver_list.py
+++ customeraddons/exam1/models/approver_list.py
@@ -2,7 +2,6 @@ from odoo import models, fields
 
 class ApproverList(models.Model):
     _name = 'approver.list'
-    _inherit= ['mail.thread']
 
     approver = fields.Many2one('res.partner' , string='Approver')
     approver_status = fields.Selection(
@@ -11,17 +10,12 @@ class ApproverList(models.Model):
          ('refuse', 'Refuse')], default='not approved yet', string='Approver Status')
     sale_order_id = fields.Many2one('plan.sale.order', string='Plan Sale Order')
     def btn_approve(self):
-        mees_approve = "approval status approved on %s" %(fields.Datetime.now())
         self.approver_status = 'approve'
         states = self.sale_order_id.approve_id.mapped('approver_status')
-        if all([state == 'approve'] for state in states):
-            self.message_post(subject='Approve New Plan', body=mees_approve)
+        if all([state == 'approve' for state in states]):
             self.sale_order_id.is_confirm = True
-
     def btn_refuse(self):
-        mess_refuse = "approval status was denied on %s" % (fields.Datetime.now())
         self.approver_status = 'refuse'
         states = self.sale_order_id.approve_id.mapped('approver_status')
-        if all([state == 'refuse'] for state in states):
-            self.message_post(subject='Refuse New Plan', body=mess_refuse)
+        if ([state == 'refuse'] for state in states):
             self.sale_order_id.is_confirm =False
--- customeraddons/exam1/models/plan_sale_oder.py
+++ customeraddons/exam1/models/plan_sale_oder.py
@@ -17,39 +17,39 @@ class PlanSaleOder(models.Model):
         self.approve_id.approver_status = 'not approved yet'
 
     def btn_send(self):
-        mess_send = 'the new plan has been sent to the person in charge by email on %s . Created by %s' %(fields.Datetime.now(), self.create_uid.name)
+        mess_send = 'kế hoạch mới đã được gửi đến người phụ trách qua email vào ngày %s . Tạo bởi %s' %(fields.Datetime.now(), self.create_uid.name)
         if self.state == 'new' or self.state == 'refuse':
             if self.approve_id.approver:
                 self.state = 'send'
-                self.message_post(subject='Send to approver', body=mess_send)
+                self.message_post(subject='Gửi tới người phê duyệt', body=mess_send)
             else:
-                raise UserError('This plan does not have any approvers')
+                raise UserError('Kế hoạch này không có bất kỳ người phê duyệt')
         else:
-            raise UserError('Cannot send this approver')
+            raise UserError('Không thể gửi người phê duyệt này')
     def btn_confirm_approve(self):
-        mess_approve = "The new plan of %s has been approved on %s" % (self.create_uid.name, fields.Datetime.now())
+        mess_approve = "Kế hoạch mới của %s đã được phê duyệt vào %s" % (self.create_uid.name, fields.Datetime.now())
         if self.state == 'send':
             if self.is_confirm:
                 if self.approve_id.approver:
                     self.state = 'approve'
                     self.message_post(subject='Approve New Plan', body=mess_approve)
                 else:
-                    raise UserError('Please write your approvers.')
+                    raise UserError('Vui lòng viết người phê duyệt của bạn.')
             else:
-                raise UserError('Cannot confirm this approve. Please check your data.')
+                raise UserError('Không thể xác nhận phê duyệt này. Vui lòng kiểm tra dữ liệu của bạn.')
         else:
-            raise UserError('user has not send')
+            raise UserError('người dùng chưa gửi yêu cầu')
     def btn_confirm_refuse(self):
-        mess_refuse = "The new plan of %s has been refused on %s" % (self.create_uid.name, fields.Datetime.now())
+        mess_refuse = "Kế hoạch mới của %s đã bị từ chối vào %s" % (self.create_uid.name, fields.Datetime.now())
         if self.state == 'send':
             if not self.is_confirm:
                 self.state ='refuse'
                 self.approve_id.approver_status='not approved yet'
                 self.message_post(subject='Refuse New Plan', body=mess_refuse)
             else:
-                raise UserError('Cannot confirm this approve. Please check your data.')
+                raise UserError('Không thể xác nhận phê duyệt này. Vui lòng kiểm tra dữ liệu của bạn.')
         else:
-            raise UserError('user has not send')
+            raise UserError('người dùng chưa gửi yêu cầu')
     #chỉ người tạo mới có thể nhìn thấy nút send
     @api.depends('create_uid')
     def _compute_check_send(self):
